076c5ad935c111d5e9187aad7d9d011c463a6346
SagarMPatil <sagarng31@gmail.com>
2026-06-04 17:10:51 +0530

    handling form with useRef

--- src/components/Auth/LoginPage.jsx
+++ src/components/Auth/LoginPage.jsx
@@ -1,13 +1,25 @@
+/* eslint-disable no-unused-vars */
 import { useRef } from "react";
 import "./LoginPage.css";
 
 const LoginPage = () => {
-  const passwordRef = useRef(null);
+  // const passwordRef = useRef(null);
+  const nameRef = useRef(null);
+  const phoneRef = useRef(null);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const user = {
+      name: nameRef.current.value,
+      phone: parseInt(phoneRef.current.value),
+    };
+    console.log(user);
+  };
 
   return (
     <>
       <section className="align_center form_page">
-        <form action="" className="authentication_form">
+        <form action="" className="authentication_form" onSubmit={handleSubmit}>
           <h2>Login Form</h2>
           <div className="form_inputs">
             <div>
@@ -16,6 +28,7 @@ const LoginPage = () => {
                 type="text"
                 name=""
                 id="name"
+                ref={nameRef}
                 className="form_text_input"
                 placeholder="Enter Name"
               />
@@ -26,11 +39,12 @@ const LoginPage = () => {
                 type="number"
                 name=""
                 id="phone"
+                ref={phoneRef}
                 className="form_text_input"
                 placeholder="Enter Phone Number"
               />
             </div>
-            <div>
+            {/* <div>
               <label htmlFor="password">Password</label>
               <input
                 type="password"
@@ -52,7 +66,7 @@ const LoginPage = () => {
               >
                 Hide
               </button>
-            </div>
+            </div> */}
             <button type="submit" className="search_button form_submit">
               Submit
             </button>
