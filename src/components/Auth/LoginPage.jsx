82545ac11f0d434084616ea24a6eb47c0ba30b93
SagarMPatil <sagarng31@gmail.com>
2026-06-06 17:23:30 +0530

    form validation

--- src/components/Auth/LoginPage.jsx
+++ src/components/Auth/LoginPage.jsx
@@ -1,29 +1,43 @@
 /* eslint-disable no-unused-vars */
 import { useRef, useState } from "react";
+import { Form, useForm } from "react-hook-form";
 import "./LoginPage.css";
 
 const LoginPage = () => {
-  const [user, setUser] = useState({
-    name: "",
-    phone: 0,
-  });
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  // console.log(formState.errors);
+
+  // const [user, setUser] = useState({
+  //   name: "",
+  //   phone: 0,
+  // });
   // const passwordRef = useRef(null);
   // const nameRef = useRef(null);
   // const phoneRef = useRef(null);
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    // const user = {
-    //   name: nameRef.current.value,
-    //   phone: parseInt(phoneRef.current.value),
-    // };
-    console.log(user);
-  };
+  // const handleSubmit = (e) => {
+  //   e.preventDefault();
+  //   // const user = {
+  //   //   name: nameRef.current.value,
+  //   //   phone: parseInt(phoneRef.current.value),
+  //   // };
+  //   console.log(user);
+  // };
+
+  const onSubmit = (formData) => console.log(formData);
 
   return (
     <>
       <section className="align_center form_page">
-        <form action="" className="authentication_form" onSubmit={handleSubmit}>
+        <form
+          action=""
+          className="authentication_form"
+          onSubmit={handleSubmit(onSubmit)}
+        >
           <h2>Login Form</h2>
           <div className="form_inputs">
             <div>
@@ -35,9 +49,16 @@ const LoginPage = () => {
                 // ref={nameRef}
                 className="form_text_input"
                 placeholder="Enter Name"
-                onChange={(e) => setUser({ ...user, name: e.target.value })}
-                value={user.name}
+                // onChange={(e) => setUser({ ...user, name: e.target.value })}
+                // value={user.name}
+                {...register("name", { required: true, minLength: 3 })}
               />
+              {errors.name?.type === "required" && (
+                <em className="form_error">Please Enter Name</em>
+              )}
+              {errors.name?.type === "minLength" && (
+                <em className="form_error">Minimum 3 characters required</em>
+              )}
             </div>
             <div>
               <label htmlFor="phone">Phone Number</label>
@@ -48,11 +69,18 @@ const LoginPage = () => {
                 // ref={phoneRef}
                 className="form_text_input"
                 placeholder="Enter Phone Number"
-                onChange={(e) =>
-                  setUser({ ...user, phone: parseInt(e.target.value) })
-                }
-                value={user.phone}
+                // onChange={(e) =>
+                //   setUser({ ...user, phone: parseInt(e.target.value) })
+                // }
+                // value={user.phone}
+                {...register("phone", {
+                  valueAsNumber: true,
+                  required: true,
+                })}
               />
+              {errors.name?.type === "required" && (
+                <em className="form_error">Please Phone Number</em>
+              )}
             </div>
             {/* <div>
               <label htmlFor="password">Password</label>
